4ff182714f45c5e3909d60aba1f642097b221b7d
徐军 <xujun@pingpongx.com>
2022-05-31 17:16:27 +0800

    feat: init

---
+++ src/controller/api.controller.ts
@@ -0,0 +1,31 @@
+import {
+  Inject,
+  Controller,
+  Get,
+  Query,
+  Post,
+  Files,
+  Fields,
+} from '@midwayjs/decorator';
+import { Context } from '@midwayjs/koa';
+import { UserService } from '../service/user.service';
+
+@Controller('/api')
+export class APIController {
+  @Inject()
+  ctx: Context;
+
+  @Inject()
+  userService: UserService;
+
+  @Get('/get_user')
+  async getUser(@Query('uid') uid) {
+    const user = await this.userService.getUser({ uid });
+    return { success: true, message: 'OK', data: user };
+  }
+
+  @Post('/upload')
+  async upload(@Files() files, @Fields() fields) {
+    return { files, fields };
+  }
+}
